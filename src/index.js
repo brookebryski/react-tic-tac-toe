dc85040b091f96a66d3e4bd96fe16aee1d7f1fa7
github username <github email address>
2022-02-03 10:36:00 -0600

    changed status variable to utilize state

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ class Board extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
